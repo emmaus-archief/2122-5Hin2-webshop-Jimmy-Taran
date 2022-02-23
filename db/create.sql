c629c743f89d29babbbe9f8bdde85f887441bf11
Jimmy Tran <76182882+Jimmy-Tran@users.noreply.github.com>
2022-02-23 11:09:09 +0000

    database added material

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,8 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  material TEXT
 );
 
 
@@ -20,7 +21,9 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('SLANGY ORNATA V2', 'Mecha-membrane keyboard with Slangy Chroma RGB', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('SLANGY CYNOSA V2', 'Membrane Gaming Keyboard with Razer Chroma RGB', '077030122-3', 11);
-insert into products (name, description, code, price) values ('SLANGY TARTARUS', 'Specialized gaming keypads with 32 fully programmable keys that are engineered to perfectly fit your hand for ultimate control.', '445924201-X', 13.5);
+insert into products (name, description, code, price, material) values ('SLANGY ORNATA V2', 'Mecha-membrane keyboard with Slangy Chroma RGB', '816905633-0', 10.5, 'ALUMINIUM');
+  
+insert into products (name, description, code, price, material) values ('SLANGY CYNOSA V2', 'Membrane Gaming Keyboard with Razer Chroma RGB', '077030122-3', 11, 'ALUMINIUM');
+  
+insert into products (name, description, code, price, material) values ('SLANGY TARTARUS', 'Specialized gaming keypads with 32 fully programmable keys that are engineered to perfectly fit your hand for ultimate control.', '445924201-X', 13.5, 'ALUMINIUM');
 
