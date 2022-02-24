3fdd550f07a3f931a2e181378c84a652e626f7ba
Jimmy Tran <76182882+Jimmy-Tran@users.noreply.github.com>
2022-02-24 01:07:25 +0000

    fixed a few flaws.

--- db/create.sql
+++ db/create.sql
@@ -21,9 +21,9 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, material) values ('SLANGY ORNATA V2', 'Mecha-membrane keyboard with Slangy Chroma RGB', '816905633-0', 10.5, 'ALUMINIUM');
+insert into products (name, description, code, price, material) values ('SLANGY ORNATA V2', 'Mecha-membrane keyboard with Slangy Chroma RGB', '816905633-0', 100, 'ALUMINIUM');
   
-insert into products (name, description, code, price, material) values ('SLANGY CYNOSA V2', 'Membrane Gaming Keyboard with Razer Chroma RGB', '077030122-3', 11, 'ALUMINIUM');
+insert into products (name, description, code, price, material) values ('SLANGY CYNOSA V2', 'Membrane Gaming Keyboard with Razer Chroma RGB', '077030122-3', 125, 'ALUMINIUM');
   
-insert into products (name, description, code, price, material) values ('SLANGY TARTARUS', 'Specialized gaming keypads with 32 fully programmable keys that are engineered to perfectly fit your hand for ultimate control.', '445924201-X', 13.5, 'ALUMINIUM');
+insert into products (name, description, code, price, material) values ('SLANGY TARTARUS', 'Specialized gaming keypads with 32 fully programmable keys that are engineered to perfectly fit your hand for ultimate control.', '445924201-X', 200, 'ALUMINIUM');
 
